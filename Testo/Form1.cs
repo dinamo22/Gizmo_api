57ddb08830a72dbbb431f0ba9f384c8cda80143b
dinamo22rus@yandex.ru <dinamo22rus@yandex.ru>
2020-07-24 08:54:10 +0700

    new classes

--- Testo/Form1.cs
+++ Testo/Form1.cs
@@ -1,16 +1,8 @@
-﻿using Microsoft.Win32.SafeHandles;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Helpers;
 using System.Windows.Forms;
 
 
@@ -36,6 +28,16 @@ namespace Testo
         public class Gizmo_api_products_full
         {
             public List<Gizmo_api_products> result { get; set; }
+            public int httpStatusCode { get; set; }
+        }
+        public class Gizmo_api_usersessions_activeinfo_full
+        {
+            public List<Gizmo_api_usersessions_activeinfo> result { get; set; }
+            public int httpStatusCode { get; set; }
+        }
+        public class Gizmo_api_user_userId_balanse_full
+        {
+            public List<Gizmo_api_user_userId_balanse> result { get; set; }
             public int httpStatusCode { get; set; }
         }
 
@@ -110,20 +112,55 @@ namespace Testo
             public string createdTime { get; set; }
             public object id { get; set; }
         }
-
+        public class Gizmo_api_usersessions_activeinfo
+        {
+            public string username { get; set; }
+            public int userId { get; set; }
+            public double span { get; set; }
+            public string lastLogin { get; set; }
+            public string lastLogout { get; set; }
+            public int hostId { get; set; }
+            public string hostName { get; set; }
+            public int hostNumber { get; set; }
+            public string userGroupName { get; set; }
+            public int userGroupId { get; set; }
+            public string hostGroupName { get; set; }
+            public int hostGroupId { get; set; }
+            public int sessionState { get; set; }
+            public int slot { get; set; }
+        }
+        public class Gizmo_api_user_userId_balanse
+        {
+            public int userId { get; set; }
+            public int deposits { get; set; }
+            public int points { get; set; }
+            public int onInvoices { get; set; }
+            public int onInvoicedUsage { get; set; }
+            public int onUninvoicedUsage { get; set; }
+            public int timeProduct { get; set; }
+            public int timeFixed { get; set; }
+            public int availableTime { get; set; }
+            public int availableCreditedTime { get; set; }
+            public int balance { get; set; }
+            public int timeProductBalance { get; set; }
+            public int usageBalance { get; set; }
+            public int totalOutstanding { get; set; }
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
             try
             {
-                //WebRequest request = WebRequest.Create("http://192.168.1.59/api/hosts");
-                WebRequest request = WebRequest.Create("http://192.168.1.59/api/users");
-                request.Credentials = new NetworkCredential("admin", "admin");
+                //WebRequest request = WebRequest.Create("http://netstorage/api/hosts");
+                //WebRequest request = WebRequest.Create("http://netstorage/api/users");
+                WebRequest request = WebRequest.Create("http://netstorage/api/usersessions/activeinfo");
+                WebRequest request1 = WebRequest.Create("http://netstorage/api/users/balance");
+                request.Credentials = new NetworkCredential("Earlies", "Vjzctcnhf1");
                 WebResponse response = request.GetResponse();
                 Stream stream = response.GetResponseStream();
                 StreamReader stream_reader = new StreamReader(stream);
                 string all_json_text = stream_reader.ReadToEnd();
-
+                
                 //Gizmo_api_host_full temp_api = JsonConvert.DeserializeObject<Gizmo_api_host_full>(all_json_text);
                 Gizmo_api_users_full temp_users = JsonConvert.DeserializeObject<Gizmo_api_users_full>(all_json_text);
                 //foreach (Gizmo_api_hosts host in temp_api.result)
